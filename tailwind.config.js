71a1fdb21ff07d1617dfd8c14e868118676ee89a
EyadOmar <eiad.omar88@gmail.com>
2023-12-26 21:50:20 +0200

    added custome colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#E78C3B',
+      secondary: '#273D5D',
+      black: '#3D3D3D',
+      'gray-dark': '#666666',
+      'gray-light': '#3165AE',
+      'gray-lighter': '#273D5D80',
+      white: '#ffffff',
+    },
   },
   plugins: [],
 };
